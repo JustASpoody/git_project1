d72f3abe4332dde6e7443863b9e27d3074a0dbee
JustASpoody <92946924+JustASpoody@users.noreply.github.com>
2022-01-16 20:19:21 +0500

    Add files via upload

---
+++ laser.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, pos, screen_height):
+        super().__init__()
+        self.image = pygame.Surface((4, 20))
+        self.image.fill('green')
+        self.speed = -5
+        self.screen_height = screen_height
+        self.rect = self.image.get_rect(center=pos)
+
+    def update(self):
+        self.rect.y += self.speed
